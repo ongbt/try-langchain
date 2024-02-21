5c59af2db56e3e0b649331c2cfc170cdaff1de27
BT Ong <bengtiong@email.com>
2024-02-21 22:18:07 +0800

    Refactored

--- pdf-read/query_data.py
+++ pdf-read/query_data.py
@@ -29,17 +29,28 @@ Answer the question based only on the following context:
 Answer the question based on the above context: {question}
 """
 
-
-def main():
+def get_query_text():
     # Create CLI.
     parser = argparse.ArgumentParser()
     parser.add_argument("query_text", type=str, help="The query text.")
     args = parser.parse_args()
-    query_text = args.query_text
+    return args.query_text
 
+
+def main():
+    query_text = get_query_text()
+    response_text, results = run(CHROMA_PATH, PROMPT_TEMPLATE, query_text)
+    parse_response(response_text=response_text, results=results)
+
+def parse_response(response_text, results):
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+
+def run(chroma_path, prompt_template, query_text):
     # Prepare the DB.
     embedding_function = OllamaEmbeddings()
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+    db = Chroma(persist_directory=chroma_path, embedding_function=embedding_function)
 
     # Search the DB.
     results = db.similarity_search_with_relevance_scores(query_text, k=3)
@@ -52,16 +63,15 @@ def main():
         return
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt_template = ChatPromptTemplate.from_template(prompt_template)
     prompt = prompt_template.format(context=context_text, question=query_text)
     print(prompt)
 
     model = Ollama()
+
     response_text = model.invoke(prompt)
 
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
-    formatted_response = f"Response: {response_text}\nSources: {sources}"
-    print(formatted_response)
+    return response_text, results
 
 
 if __name__ == "__main__":
